c747cdcdf9c6571ff63de073d0d53c85011014b9
issamhxr2 <148063427+Issamhxr@users.noreply.github.com>
2024-04-07 06:07:11 +0200

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -12,7 +12,7 @@ export const verifyToken = async (req, res, next) => {
       token = token.slice(7, token.length).trimLeft();
     }
 
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    const verified = jwt.verify(token, process.env.CLE);
     req.user = verified;
     next();
   } catch (err) {
